8aee6409499e18e1e84788c18fa9754fd547e072
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-06-12 19:24:46 +0200

    counting a number from multilpying from tuple

---
+++ tuple1.py
@@ -0,0 +1,6 @@
+numbers = (2, 3, 5, 7, -11, 13, 17, 19, 23, 29, 31, -6, 41, 43, 47, 53, 59, 61, -96, 71, 1000, -1)
+str1 = list(numbers)
+count = 1
+for i in range(len(str1)):
+    count *= int(str1[i])
+print(count)
