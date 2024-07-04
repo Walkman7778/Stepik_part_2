6f511f161f5a578935b18582e99d537834f43d9b
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-07-04 05:43:09 +0200

    secret word

--- secret_word.py
+++ secret_word.py
@@ -1,3 +1,13 @@
 a = list(input())
-s = dict(zip(a, a.count(list(a))))
-print(s)
+s = list(a)
+dict1 = dict(zip(list(range(len(a))), a))
+dict2 = {}
+n = int(input())
+b = [input().split(": ") for _ in range(n)]
+dict2 = dict(zip([i[1] for i in b], [i[0] for i in b]))
+for key, value in dict1.items():
+    if a.count(value) == int(dict2[key]):
+        print(dict2[key])
+print(dict1)
+print(dict2)
+
