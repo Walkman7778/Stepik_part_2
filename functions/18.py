afd0dca2514117ebd54622205086a17cec78282e
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-08-19 10:56:18 +0200

    18.py

---
+++ functions/18.py
@@ -0,0 +1,5 @@
+# task in russian here --- https://stepik.org/lesson/512854/step/5?unit=505068
+def func(x,):
+    return bool(list(filter(lambda x: (x % 19 == 0 or x % 13 == 0), [x])))
+# Creating a  func, using syntax of anonymous func, which get int argument and return True, if it odd 19 or 13 otherwise 
+# return False 
