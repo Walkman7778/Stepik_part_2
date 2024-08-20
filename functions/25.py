45cd9e855c46a94adc95901e10dc8da3e303f068
Trinkinet Piotr <ptrinkinet@gmail.com>
2024-08-20 17:51:37 +0200

    25

---
+++ functions/25.py
@@ -0,0 +1,3 @@
+# task in russian here --- https://stepik.org/lesson/512854/step/15?unit=505068
+x = [str(i) for i in input().split()]
+print(list(map(lambda x : 255 - int(x[0]), 255 - int(x[1]), 255 - int(x[2]) , x)))
